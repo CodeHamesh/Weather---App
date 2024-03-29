ee1451e703fec9773c4209ebd8dcf8eefdd7340f
codehamesh <work.hamesh@gmail.com>
2024-03-29 12:04:09 +0530

    add

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 let url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ecf318d8ce8e73ae6560907cd686b8af"
-
-async function wApp(city){
-    let f = await fetch(url + `&q=${city}` + `&units=metric`)
-    let j = await f.json();
-    return j
-
-}
-wApp("jaipur")
-
-
-;
-
 let inp = document.querySelector("input")
 let btn = document.querySelector("button")
 let p = document.querySelector("p")
+async function wApp(city){
+    let f = await fetch(url + `&q=${city}` + `&units=metric`)
+    let j = await f.json();
+    console.log(j);
+    p.textContent = j.main.temp + " °C"
+
+}
 
 btn.addEventListener("click",()=>{
-   
+   wApp(inp.value)
+   inp.value = ''
+})
+
+inp.addEventListener("keydown",(e)=>{
+   if (e.keyCode === 13) {
+    wApp(inp.value)
+    inp.value = ''
+   }
 })
 
 
-function name(params) {
-    let add = 2*2
-    return add
-}
+
+
+
